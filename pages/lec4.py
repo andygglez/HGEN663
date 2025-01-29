a47fd6c0cfee416b4a5c56d5a5733c2bea3eb84f
andy-bio <andyandygggg@gmail.com>
2025-01-28 19:50:49 -0500

    Lec 4

--- pages/lec4.py
+++ pages/lec4.py
@@ -8,193 +8,124 @@ with col2:
     #############################################################################################
 
     with st.container(border=True):
-            st.markdown("#### Copy files for part 1 from `/home/hgen_share/lec4_pt1`")
-            
-            st.markdown("Set up directory and copy over files")
-            st.code("""
-            module load StdEnv/2020 gcc/9.3.0 openmpi/4.0.3 hdf5/1.10.6 minimap2/2.17 fastqc/0.11.9 samtools/1.11 mugqic/qualimap/2.2.1 mafft/7.471 emboss/6.6.0 fasttree/2.1.11 mugqic/pycoQC/2.5.2
-            export PATH="/home/hgen_share/Anaconda/bin:$PATH"
-            export PATH="/home/hgen_share/Anaconda/envs/longread_env/bin:$PATH"
-            export PATH=/home/hgen_share/ont-guppy-cpu/bin:$PATH
-            """, language="bash")
+        st.markdown("#### In this first part of the class we give a general description of the tools that were used to call variants with the PacBio data")
+        st.markdown("NOTE: :red[You won't be able to run this code!!!]")
 
-    st.divider()
-    #############################################################################################
+        st.markdown("##### Alignment")
+        st.code("""
+        pbmm2 align -j 10 ${REF_FILE} \\
+                ${BASEDIR}/RS_PacBio_WGS_202X/controls/fastq/R0040235.fastq R0040235.sorted.bam \\
+                --sort --rg '@RG\tID:idR0040235\tSM:myR0040235'
+        """, language="bash")
+        
+        st.markdown("##### Call variants")
+        st.code("""
+        singularity exec --bind /usr/lib/locale/:/usr/lib/locale/ \\
+                --bind /lb/scratch/ebareke/tmp/:/lb/scratch/ebareke/tmp/ \\
+                --bind ${BASEDIR}:/data/ \\
+                --workdir /data/RS_PacBio_WGS_202X/tmp/R0040235 \\
+                --writable-tmpfs \\
+                ${BASEDIR}/RS_PacBio_WGS_202X/images/deepvariant_1.8.0.sif /opt/deepvariant/bin/run_deepvariant \\
+                --model_type=PACBIO \\
+                --ref /data/RS_PacBio_WGS_202X/ref/hg38.fasta \\
+                --reads /data/RS_PacBio_WGS_202X/reads/ALIGNED/R0040235.sorted.bam \\
+                --vcf_stats_report=true \\
+                --output_vcf=/data/RS_PacBio_WGS_202X/final/hg38/SNV/${SAMPLE}.vcf.gz \\
+                --output_gvcf=/data/RS_PacBio_WGS_202X/final/hg38/SNV/${SAMPLE}.g.vcf.gz \\
+                --intermediate_results_dir=/data/RS_PacBio_WGS_202X/intermediate_results_dir/${SAMPLE} \\
+                --num_shards=12
+        """, language="bash")
+           
+        st.markdown("##### Call CNVs")
+        st.code("""
+        hificnv --bam ${INPUT_DIR}/${SAMPLE}.sorted.bam \\
+                --maf ${MAF_FILE} \\
+                --ref ${REF_FILE} \\
+                --exclude ${EXCLUDE_REGION} \\
+                --expected-cn ${EXPECTED_CN} \\
+                --threads 4 \\
+                --output-prefix ${SAMPLE}
+        """, language="bash")
+        
+        st.markdown("##### Call SVs")
+        st.code("""
+        pbsv discover ${BASEDIR}/RS_PacBio_WGS_202X/reads/ALIGNED/R0040235.sorted.bam R0040235.svsig.gz
+        tabix -c '#' -s 3 -b 4 -e 4 R0040235.svsig.gz
+        pbsv call -j 10 ${REF_FILE} R0040235.svsig.gz R0040235.SV.vcf
+        """, language="bash")
 
-    with st.container(border=True):
-            st.markdown("#### Take a look at ex.fast5")
-            
-            st.markdown("View the file with `h5ls`")
-            st.code("""
-            h5ls ex.fast5 | less
-            """, language="bash")
+        st.markdown("##### Genotype repeats")
+        st.code("""
+        trgt genotype --genome ${REF_FILE} \\
+                --repeats ${REPEAT_DEF} \\
+                --reads ${INPUT_DIR}/${SAMPLE}.sorted.bam \\
+                --output-prefix ${SAMPLE}
+        """, language="bash")
 
-            st.markdown("Show structure of one read using `h5ls`")
-            st.code("""
-            h5ls -r ex.fast5/read_00017b75-18fc-40ae-8a21-a5b3e49dc753
-            """, language="bash")
 
-    st.divider()
-    #############################################################################################
+    if "authenticated" not in st.session_state:
+        st.session_state.authenticated = False
+        
+    if not st.session_state.authenticated:
+        st.markdown("#### Password Protected Section")
+        password = st.text_input("Enter the password: ", type="password")
+        PASSWORD="bcftools"
 
-    with st.container(border=True):
-            st.markdown("#### Basecall using Guppy")
-            
-            st.markdown("This will take a while with just 1 CPU thread, and since the output is provided already, you can terminate it prematurely with `Ctrl-C`")
-            st.code("""
-            conf="dna_r9.4.1_450bps_hac"
+        if st.button("Submit"):
+            if password == PASSWORD:
+                st.session_state.authenticated = True
+                st.success("Access granted!")
+            else:
+                st.error("Invalid password. Please try again.")
 
-            guppy_basecaller \\
-                --compress_fastq \\
-                -i ex.fast5 \\
-                -s basecall/ \\
-                --cpu_threads_per_caller 1 \\
-                --num_callers 1 \\
-                -c dna_r9.4.1_450bps_hac.cfg
-            """, language="bash")
+    else:
 
-    st.divider()
-    #############################################################################################
+        st.divider()
+        with st.container(border=True):
+            st.markdown("#### Compare Illumina vs PacBio SNVs: ")
 
-    with st.container(border=True):
-            st.markdown("#### (Extra) Assess quality with pycoQC & FastQC")
-            
-            st.markdown("Run `pycoQC` with the provided summary")
-            st.code("""
-            pycoQC -f sequencing_summary.txt -o pyco.html
-            """, language="bash")
-
-            st.markdown("Run `FastQC` with the provided sequences")
-            st.code("""
-            fastqc ex.fq
-            """, language="bash")
-
-            st.markdown("Download `pycoQC` html report")
-            st.markdown("Compare with another report [here](https://a-slide.github.io/pycoQC/pycoQC/results/Guppy-2.3_basecall-1D_alignment-DNA.html)")
-
-    st.divider()
-    #############################################################################################
-
-    with st.container(border=True):
-            st.markdown("#### (Extra) Rectify quality issues by trimming")
-            
-            st.markdown("Trim adapters with `Porechop`")
-            st.code("""
-            porechop -i ex.fq -t 1 -v 2 -o chopped.fq
-            """, language="bash")
-
-            st.markdown("Is it better now? Run fastQC on trimmed `fastq`")
-
-    st.divider()
-    #############################################################################################
-
-    with st.container(border=True):
-            st.markdown("#### (Extra) Assess error profile after alignment")
-            
-            st.markdown("Align trimmed reads using `minimap2`")
-            st.code("""
-            minimap2 -t 1 -x map-ont -a wuhCor1.fa chopped.fq | samtools sort -o ex.bam
-            samtools index ex.bam
-            """, language="bash")
-
-            st.markdown("Summarize alignment results with `Qualimap`")
-            st.code("""
-            qualimap bamqc -bam ex.bam --java-mem-size=1G -nw 5000 -nt 1 -c -outdir qualimap/
-            """, language="bash")
-
-            st.markdown("Download results to your local computer")
-            st.markdown("Compare with the [report](https://rawgit.com/kokonech/kokonech.github.io/master/qualimap/HG00096.chr20_bamqc/qualimapReport.html) from a typical Illumina run")
-    st.divider()
-    #############################################################################################
-
-    with st.container(border=True):
-            st.markdown("#### Hybrid assembly with Unicycler")
-            
-            st.markdown("Set up variables and copy over files for part 2")
-            st.code("""
-            export PATH=/home/hgen_share/utils/SPAdes-3.15.5-Linux/bin:$PATH
-            export PATH=/home/hgen_share/utils/ncbi-blast-2.13.0+/bin:$PATH
-            export PATH=/home/hgen_share/utils/racon/build/bin:$PATH
-            export PATH="/home/hgen_share/Anaconda/bin:$PATH"
-            export PATH="/home/hgen_share/Anaconda/envs/longread_env/bin:$PATH"
-            cp /home/hgen_share/lec4_pt2/* ./
-            """, language="bash")
-
-            st.markdown("""Run `Unicycler` using boths short- and long-reads. 
-            For hybrid assemblies, the direct long-read bridging step of the
-            pipeline can take a while to complete. Thus, the output is provided
-            for you. You can terminate it prematurely with `Ctrl-C`""")
-            st.code("""
-            unicycler -1 Illumina_F.fq -2 Illumina_R.fq \\
-                    -l ONT.fq -t 20 --min_fasta_length 10000 \\
-                    --linear_seqs 1 -o unicycler
-            """, language="bash")
-
-            st.markdown("The output can be found in `/home/hgen_share/lec4_pt2/unicycler`")
-    st.divider()
-    #############################################################################################
-
-    with st.container(border=True):
-            st.markdown("#### Evaluate the assembled genome's quality")
-            
-            st.markdown("Use `QUAST` to create a summary report")
-            st.code("""
-            module load mugqic/Quast/5.0.2
-            quast.py -t 1 -o quast -R wuhCor1.fa -g wuhCor1.gff3 unicycler/assembly.fasta
-            """, language="bash")
-
-            st.markdown("Compare with a [report](http://cab.cc.spbu.ru/quast/paper/h.sapiens_chr14/) on eukaryotic assemblies")
-
-            st.markdown("[BLAST](https://blast.ncbi.nlm.nih.gov/Blast.cgi) the top 2 contigs (`contig1.fa` and `contig2.fa`) found in the `unicycler` output directory")
-
-    st.divider()
-    #############################################################################################
-
-    with st.container(border=True):
-            st.markdown("#### Place newly assembled viral genomes in a global context")
-            
-            st.markdown("Set up variables for nextstrain environment")
-            st.code("""
-            export PATH="/home/hgen_share/Anaconda/bin:$PATH"
-            export PATH="/home/hgen_share/Anaconda/envs/nextstrain_V2/bin:$PATH"
-            """, language="bash")
-
-            st.markdown("Copy the tutorial dataset for SARS-Cov2 provided by nextstrain")
-            st.code("""
-            cp -r /home/hgen_share/ncov ./
-            """, language="bash")
-
-            st.markdown("""Take a look at the `/data` sub-directory. Inspect the data 
-            files with `less` to get an impression of what data needs to be provided 
-            for nextstrain. Unzip the fasta file using `gunzip`""")
-            st.markdown("Go back to the main `/ncov` directory and run the basic nextstrain workflow")
+            st.markdown("Load modules")
+            st.code("module load bcftools/1.19")
 
             st.code("""
-            snakemake --cores 14 --profile ./my_profiles/getting_started
+            ill_snv=/project/def-sponsor00/hgen_share/lec4/HG002.Illumina.dv.vcf.gz
+            pacbio_snv=/project/def-sponsor00/hgen_share/lec4/HG002_PacBio_GRCh38.deepvariant.phased.chr22.vcf.gz
+
+            # Only present in the first file
+            user=/project/def-sponsor00/$USER/lec4
+
+            bcftools isec -C ${ill_snv} ${pacbio_snv} -o ${user}/snv.illumina.uniq.txt
+            bcftools isec -C ${pacbio_snv} ${ill_snv} -o ${user}/snv.pacbio.uniq.txt
+
+            # Present in both
+            bcftools isec -n=2 ${pacbio_snv} ${ill_snv} -o ${user}/snv.common.txt
             """, language="bash")
 
-            st.markdown("""On your own computer, not the cluster, establish a tunnel 
-            to server. Here you should use your own username.Also, replace the hashtag
-            with any two numbers to land on a port that is not busy""")
-            st.code("""
-            ssh -L 40##:localhost:40## studXX@workshop2021a.vhost37.genap.ca
-            """, language="bash")
-
-            st.markdown("""Start the auspice server to view the results using your credentials.
-            If the PORT is busy, change to another port by exporting the port number as indicated below""")
-            st.code("""
-            export PORT=40##
-            auspice view --datasetDir /home/studXX/projects/lec4/ncov/auspice
-            """, language="bash")
-
-            st.markdown("You can visit the results page at [](http://localhost:40##) in your browser. If you're done inspecting the results, terminate the auspice server with `Ctrl+C`")
-            st.markdown("""Include our own recently assembled data. Use `consensus_sequences.fasta`
-            and `metadata.tsv` from `/home/hgen_share/lec4_pt2`. Append the fasta file to the example
-            sequences (you can also inspect the fasta file with `less`""")
+            st.markdown("#### Now compare Illumina vs PacBio SVs: ")
 
             st.code("""
-            cat consensus_sequences.fasta >> data/example_sequences.fasta
+            ill_sv=/project/def-sponsor00/hgen_share/lec4/HG002_Illumina_GRCh38.manta.chr22.vcf.gz
+            pacbio_sv=/project/def-sponsor00/hgen_share/lec4/HG002_PacBio_GRCh38.pbsv.phased.chr22.vcf.gz
+
+            # Only present in the first file
+            bcftools isec -C ${ill_sv} ${pacbio_sv} -o ${user}/sv.illumina.uniq.txt
+            bcftools isec -C ${pacbio_sv} ${ill_sv} -o ${user}/sv.pacbio.uniq.txt
+
+            # Present in both
+            bcftools isec -n=2 ${ill_sv} ${pacbio_sv} -o ${user}/sv.common.txt
             """, language="bash")
 
-            st.markdown("Do the same for the metadata file. Append it to `example_metadata.tsv`")
-            st.markdown("Run the same steps as above. Where does our data reside in the phylogeny?")
+            st.markdown("#### Now inspect the files with unique and common sites:")
+
+            st.code("less ${user}/snv.illumina.uniq.txt", language="bash")
+            st.markdown("What can you see?")
+            st.markdown("Look at the following regions and describe what you see:")
+
+            st.markdown("""
+            :blue[#1]- chr22:36,985,075-36,985,194 \\
+            :blue[#2]- chr22:36,742,174-36,757,487 \\
+            :blue[#3]- chr22:36,447,091-36,447,130 \\
+            :blue[#4]- chr22:36,433,926-36,434,508 \\
+            :blue[#5]- chr22:36,219,565-36,219,779 \\
+            :blue[#6]- chr22:10,699,393-10,699,432
+            """)
