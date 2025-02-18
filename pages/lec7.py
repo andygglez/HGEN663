c3332de582e5da48a30a57993c1a08f3a95be547
andy-bio <andyandygggg@gmail.com>
2025-02-18 18:23:45 -0500

    etw

--- pages/lec7.py
+++ pages/lec7.py
@@ -185,39 +185,98 @@ with col2:
         st.markdown("##### Part 2")
         
         st.markdown("#### Import results from rMATS")
-        st.markdown("Import the rest of the results from rMATS (ie. those ending with ‘.JC.’, which are junction counts).")
-        st.code("""
-        se <- read.table("path/to/rmats_output_SNRPB/SE.MATS.JC.txt",sep = '\t', header = TRUE, stringsAsFactors = FALSE)
-        ri <- read.table("path/to/rmats_output_SNRPB/RI.MATS.JC.txt",sep = '\t', header = TRUE, stringsAsFactors = FALSE)
-        """, language="r")
-        st.markdown("#### Differential Splicing Events")
-        st.markdown("Which type are the most abundant differentially spliced events (DSEs)? Is there a tendancy for the knockdown samples towards one of the DSEs? (for example exon skipping?)")
-        st.code("""
-        se_dse <- se %>%
-            filter(FDR < 0.05)
-            nrow(se_dse[se_dse$IncLevelDifference < 0,]) #differential skipping in control
-            nrow(se_dse[se_dse$IncLevelDifference > 0,]) #differential skipping in knockdown
-        
-        ri_dse <- ri %>%
-            filter(FDR < 0.05)
-            nrow(ri_dse[ri_dse$IncLevelDifference > 0,]) #differential retained introns in control
-            nrow(ri_dse[ri_dse$IncLevelDifference < 0,]) #differential retained introns in knockdown
-        """, language="r")
 
-        st.markdown("Plot your results!")
-        st.code("""
-        df <- data.frame(SE = c(106,354),
-                RI = c(26,317),
-                samp = c("Control","Knockdown")) %>%
-                pivot_longer(cols = c("SE","RI"),names_to = "DSE",values_to = "count")
-        
-        df$samp <- fct_rev(df$samp)
+        st.code(
+        """
+# Before starting, make sure that following packages are loaded
+library("clusterProfiler")
+library("AnnotationDbi")
+library("org.Hs.eg.db")
 
-        df %>%
-        ggplot(aes(x=DSE,y=count,fill=samp)) +
-        geom_bar(position="stack",stat="identity") +
-        coord_flip() +
-        scale_fill_manual(values=c("purple", "red")) +
-        theme_minimal()
-        """, language="r")
+# Step 1: Change the filepath to where the downloaded "splicing_analysis_demo" file is stored on your device
+filePath <- "path/to/folder"
+        
+# Step 2: Set the cutoff criteria for filtering the RMATS results
+FDR_cutoff <- 0.05
+inclusion_diff_cutoff <- 0.1
+
+# Step 3: Load the RMATS output file titled "RI.MATS.JC.txt"
+event_file <- read.delim() ### Complete this line
+
+head(event_file) # see first few lines of the event file
+
+# Step 4: Filter for events with an FDR below the set cutoff and an inclusion level difference about cutoff
+event_file_filtered <- event_file[] ### Complete this line
+
+# Step 5: Classify if each event is more included (ie. occurs more) in the control or case condition
+event_file_filtered_control <- event_file_filtered[event_file_filtered$IncLevelDifference > inclusion_diff_cutoff,]
+event_file_filtered_case <- event_file_filtered[event_file_filtered$IncLevelDifference < -inclusion_diff_cutoff,]
+
+
+cat("Number of events prior to filtering: ", nrow(event_file),
+"\\nNumber of events after filtering on FDR: ", nrow(event_file_filtered),
+"\\nNumber of events with more intron retention in control: ", nrow(event_file_filtered_control),
+"\\nNumber of events in more intron retention in case: ", nrow(event_file_filtered_case))
+
+# Step 6: Use all events as a background in over-representation analysis
+background <- unique(event_file$geneSymbol)
+background <- mapIds(org.Hs.eg.db, keys = background, column = "ENTREZID", keytype = "SYMBOL") # converts gene symbols to Entrez ID, required for enrichGO function
+background <- background[!is.na(background)] # removes na values (no mapped Entrez ID)
+
+# Step 7: Convert our subset of significant events into a gene list
+gene_list <- unique(event_file_filtered$geneSymbol)
+gene_list <- mapIds(org.Hs.eg.db, keys = gene_list, column = "ENTREZID", keytype = "SYMBOL") # converts gene symbols to Entrez ID, required for enrichGO function
+gene_list <- gene_list[!is.na(names(gene_list))] # removes na values (no mapped Entrez ID)
+
+# Step 8: Run GO results with our background 
+GO_results_with_background <- enrichGO(
+        
+)
+
+# Step 9: Run GO results without our background
+GO_results_NO_background <- enrichGO(
+        
+)
+
+# Try changing the value of "showCategory", what happens?
+barplot(GO_results_with_background, showCategory = 10, title = "Results Using Gene Background") + 
+    barplot(GO_results_NO_background, showCategory = 10, title = "Results Without Gene Background") # shows the results of enrichGO as barplots
+
+""", language="r")
+
+        # st.markdown("Import the rest of the results from rMATS (ie. those ending with ‘.JC.’, which are junction counts).")
+        # st.code("""
+        # se <- read.table("path/to/rmats_output_SNRPB/SE.MATS.JC.txt",sep = '\t', header = TRUE, stringsAsFactors = FALSE)
+        # ri <- read.table("path/to/rmats_output_SNRPB/RI.MATS.JC.txt",sep = '\t', header = TRUE, stringsAsFactors = FALSE)
+        # """, language="r")
+        # st.markdown("#### Differential Splicing Events")
+        # st.markdown("Which type are the most abundant differentially spliced events (DSEs)? Is there a tendancy for the knockdown samples towards one of the DSEs? (for example exon skipping?)")
+        # st.code("""
+        # se_dse <- se %>%
+        #     filter(FDR < 0.05)
+        #     nrow(se_dse[se_dse$IncLevelDifference < 0,]) #differential skipping in control
+        #     nrow(se_dse[se_dse$IncLevelDifference > 0,]) #differential skipping in knockdown
+        
+        # ri_dse <- ri %>%
+        #     filter(FDR < 0.05)
+        #     nrow(ri_dse[ri_dse$IncLevelDifference > 0,]) #differential retained introns in control
+        #     nrow(ri_dse[ri_dse$IncLevelDifference < 0,]) #differential retained introns in knockdown
+        # """, language="r")
+
+        # st.markdown("Plot your results!")
+        # st.code("""
+        # df <- data.frame(SE = c(106,354),
+        #         RI = c(26,317),
+        #         samp = c("Control","Knockdown")) %>%
+        #         pivot_longer(cols = c("SE","RI"),names_to = "DSE",values_to = "count")
+        
+        # df$samp <- fct_rev(df$samp)
+
+        # df %>%
+        # ggplot(aes(x=DSE,y=count,fill=samp)) +
+        # geom_bar(position="stack",stat="identity") +
+        # coord_flip() +
+        # scale_fill_manual(values=c("purple", "red")) +
+        # theme_minimal()
+        # """, language="r")
                 
