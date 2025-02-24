827f130d059b2bdfe8e1e7bc5dc86ba7670bca79
andy-bio <andyandygggg@gmail.com>
2025-02-24 15:12:30 -0500

    Lec8 fixed

--- pages/lec8.py
+++ pages/lec8.py
@@ -18,7 +18,7 @@ with col2:
         library(patchwork)
         library(reactable)
         # set your working directory
-        setwd("~/Documents/HGEN_663/extra/lec8")
+        setwd("<path>")
         """, language="r")
 
     st.divider()
@@ -71,31 +71,35 @@ with col2:
         st.markdown("We will compute some QC metrics that reflect the overall quality of the sample: coverage (# of genes, # of UMIs) and mitochondrial read proportions – which is often an indicator of stress/cell damage")
         st.code("""
         # get a list of mitochondrial genes: all genes starting with 'MT'
-        mito.genes <- grep("^MT-", rownames(x = Seurat_object@assays$RNA), value = TRUE)
+        mito.genes <- grep("^MT-", rownames(Seurat_object[["RNA"]]), value = TRUE)
 
         # compute proportions
-        percent.mito <- Matrix::colSums(Seurat_object@assays$RNA[mito.genes,]) /
-        Matrix::colSums(Seurat_object@assays$RNA)
+        percentage_present <- function(expr_matrix, genes_vector) {
+        apply(expr_matrix, 2, function(col) {
+            present_genes <- rownames(expr_matrix)[col > 0]  # Get genes with expression > 0
+            mean(present_genes %in% genes_vector) * 100  # Calculate percentage
+        })
+        }
+        percent.mito <- percentage_present(DGEmatrix, mito.genes)
+
 
         # add the information back to the seurat object
-        Seurat_object <- AddMetaData(object = Seurat_object, metadata = percent.mito,
-                                    col.name = "percent.mito")
+        Seurat_object <- AddMetaData(Seurat_object, percent.mito, col.name = "percent.mito")
 
         # get a list of crystal genes (all genes starting by 'CRY') to remove crystalin contamination
-        crystal.genes <- grep("^CRY[AB]", rownames(x = Seurat_object@assays$RNA), value = TRUE)
+        crystal.genes <- grep("^CRY[A-Z]", rownames(Seurat_object[["RNA"]]), value = TRUE)
 
         # compute proportions
-        percent.crystal <- Matrix::colSums(Seurat_object@assays$RNA[crystal.genes, ]) / 
-        Matrix::colSums(Seurat_object@assays$RNA)
+        percent.crystal <- percentage_present(DGEmatrix, crystal.genes)
 
         # add the information back to the seurat object
-        Seurat_object <- AddMetaData(object = Seurat_object, metadata = percent.crystal,
-                                    col.name = "percent.crystal")
+        Seurat_object <- AddMetaData(Seurat_object, percent.crystal, col.name = "percent.crystal")
+
 
         # display distribution of some metrics:
         # # of genes, # of UMIs, and mitochondrial/crystal proportion
         fts <- c("nFeature_RNA", "nCount_RNA", "percent.mito", "percent.crystal")
-        VlnPlot(object = Seurat_object, ncol = 4, pt.size = 0, features = fts)
+        VlnPlot(Seurat_object, features = fts, ncol = 4, pt.size = 0)
         """, language="r")
         st.image("images/lec8.QC.png")
 
@@ -117,29 +121,56 @@ with col2:
         """, language="r")
 
         st.image("images/lec8.summary.png")
-                
-        st.markdown("")
+    
+    st.divider()
+
+    with st.container(border=True):
+        st.markdown("### Filtering")
+
+        st.code("""
+        # We'll define some cutoffs to remove cells that deviate too much from the rest of the
+        # cells in the sample. These can be hard cut-offs or dataset-specific ones based on SD.
+        # Sometimes those represent multiplets, or just abnormal cells like crystalin cells
+        dim(Seurat_object)
+        Seurat_object <-  subset(Seurat_object,  subset = nFeature_RNA > 100 & nFeature_RNA < 6000 & 
+                                nCount_RNA < 10000 & nCount_RNA < 25000 &
+                                percent.mito < 10 & percent.crystal < 2.5)
+        dim(Seurat_object)
+        """)
+        
+        st.markdown("Now look at the stats after filtering:")
+        st.code("""
+        # Get some summary stats for the sample:
+        summary_stats_after_filtering <- tibble(
+        total_cells  = nrow(Seurat_object@meta.data),
+        mean_n_genes = mean(Seurat_object@meta.data$nFeature_RNA),
+        sd_n_genes   = sd(Seurat_object@meta.data$nFeature_RNA),
+        max_n_genes  = max(Seurat_object@meta.data$nFeature_RNA),
+        min_n_genes  = min(Seurat_object@meta.data$nFeature_RNA),
+        mean_UMI     = mean(Seurat_object@meta.data$nCount_RNA),
+        sd_UMI       = sd(Seurat_object@meta.data$nCount_RNA),
+        max_UMI      = max(Seurat_object@meta.data$nCount_RNA),
+        min_UMI      = min(Seurat_object@meta.data$nCount_RNA)
+        ) %>% mutate_all(function(x) round(x, 2))
+
+        summary_stats_after_filtering %>% reactable()
+        """)
+
         st.code("""
         print(
             paste0("Number of filtered cells: ",
-                summary_stats_before_filtering$total_cells -
-                summary_stats_after_filtering$total_cells
-            )
-        )
-        """, language="r")
-        st.image("images/lec8.filt.cells.png")
-
-        st.markdown("Not much was removed, only a few outlier cells, because this was a quite good quality data to begin with. In other datasets, the before/after QC metrics will look much more different.")
+            summary_stats_before_filtering$total_cells -
+            summary_stats_after_filtering$total_cells))
+        """)
 
     st.divider()
-    #############################################################################################
 
     with st.container(border=True):
-        st.markdown("#### Normalization")
-        
-        st.markdown("Normalize the dataset using [SCTransform](https://satijalab.org/seurat/articles/sctransform_vignette.html)")
+
+        st.markdown("### Normalization")
         st.code("""
         # before proceeding, we note markers for cell cycle
+        Seurat_object <- NormalizeData(Seurat_object)
         Seurat_object <- CellCycleScoring(Seurat_object, set.ident = FALSE,
                                         s.features = cc.genes$s.genes,
                                         g2m.features = cc.genes$g2m.genes)
@@ -152,31 +183,42 @@ with col2:
         FeatureScatter(Seurat_object, feature1 = "nCount_RNA", feature2 = "percent.mito") +
         FeatureScatter(Seurat_object, feature1 = "nCount_RNA", feature2 = "nFeature_RNA") &
         theme(legend.position = 'none')
-        """, language="r")
+        """)
+
         st.image("images/lec8.normalization.png")
 
     st.divider()
+    #############################################################################################
+
+    # with st.container(border=True):
+    #     st.markdown("#### Normalization")
+        
+    #     st.markdown("Normalize the dataset using [SCTransform](https://satijalab.org/seurat/articles/sctransform_vignette.html)")
+    #     st.code("""
+    #     # before proceeding, we note markers for cell cycle
+    #     Seurat_object <- NormalizeData(Seurat_object)
+    #     Seurat_object <- CellCycleScoring(Seurat_object, set.ident = FALSE,
+    #                                     s.features = cc.genes$s.genes,
+    #                                     g2m.features = cc.genes$g2m.genes)
+
+    #     # we then go on to apply a transforms the data and regresses out unwanted variation
+    #     vs <- c("nFeature_RNA", "percent.mito", "S.Score", "G2M.Score")
+    #     Seurat_object <- SCTransform(Seurat_object, verbose = T,
+    #                                 vars.to.regress = vs)
+
+    #     FeatureScatter(Seurat_object, feature1 = "nCount_RNA", feature2 = "percent.mito") +
+    #     FeatureScatter(Seurat_object, feature1 = "nCount_RNA", feature2 = "nFeature_RNA") &
+    #     theme(legend.position = 'none')
+    #     """, language="r")
+    #     st.image("images/lec8.normalization.png")
+
+    # st.divider()
     #############################################################################################
 
     with st.container(border=True):
         st.markdown("#### Dimension reduction")
         
         st.markdown("##### PCA")
-        st.markdown("We will perform a first dimension reduction by using only the most variable genes. Then, we’ll transform the data using principal component analysis (PCA), and use only the first few components for downstream analyses.")
-        st.code("""
-        # a bit of wrangling to prepare for VariableFeaturePlot
-        Seurat_object[['SCT']]@meta.features <- SCTResults(Seurat_object[['SCT']], slot = 'feature.attributes')[, c('gmean', 'variance', 'residual_variance')]
-        Seurat_object[['SCT']]@meta.features$variable <- F
-        Seurat_object[['SCT']]@meta.features[VariableFeatures(Seurat_object[['SCT']]), 'variable'] <- F
-        colnames(Seurat_object[["SCT"]]@meta.features) <- paste0("sct.", colnames(Seurat_object[["SCT"]]@meta.features))
-
-        # label top 10 most variable features
-        VariableFeaturePlot(Seurat_object, selection.method = 'sct', assay = 'SCT') %>%
-        LabelPoints(points = head(VariableFeatures(Seurat_object), 10), repel = T) &
-        theme(legend.position = 'none')
-        """, language="r")
-        st.image("images/lec8.sc.PCA.png")
-
         st.code("""
         # perform PCA
         Seurat_object <- RunPCA(Seurat_object, pcs.compute = 100, do.print = F)
@@ -184,6 +226,8 @@ with col2:
         # display genes correlated with PCs 1 & 2
         VizDimLoadings(Seurat_object, dims = 1:2, reduction = "pca")
         """, language="r")
+
+        st.image("images/lec8.VizDimLoadings.png")
 
         st.code("""
         # alternative representation of genes highly correlated with PC1
@@ -230,7 +274,6 @@ with col2:
                                     resolution = 0.5, 
                                     reduction = "umap")
         """, language="r")
-        st.image("images/lec8.clust.png")
 
         st.markdown("UMAP")
         st.code("""
@@ -261,7 +304,7 @@ with col2:
                                         min.pct = 0.25, logfc.threshold = 0.25)
         head(Seurat_object.markers) %>% reactable()
         """, language="r")
-        st.image("images/lec8.table.png")
+        # st.image("images/lec8.table.png")
 
         st.markdown("We can take a look at the top 10 markers in each cluster")
         st.code("""
