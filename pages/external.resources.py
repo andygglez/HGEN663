55e7ad93c892fbcc11428edb5ac908984d09b924
andy-bio <andyandygggg@gmail.com>
2025-04-10 11:46:46 -0400

    Website finished

--- pages/external.resources.py
+++ pages/external.resources.py
@@ -16,9 +16,10 @@ st.markdown("[RStudio](https://posit.co/download/rstudio-desktop/)")
 
 
 st.markdown("# Videos")
-st.markdown("## Lecture 1")
 
+st.markdown("## Lecture 1")
 col1,col2,col3,col4,col5 = st.columns(5)
+
 with col1:
     st.video("https://www.youtube.com/watch?v=EMaFdfIlK58&list=PLevQaddMWNQyWW-xbu5WXqwgEV20ydVut")
 
@@ -53,7 +54,6 @@ with col10:
 
 
 st.markdown("## Lecture 3")
-
 col11,col12,col13, col14,col15 = st.columns(5)
 
 with col11:
@@ -71,181 +71,174 @@ with col14:
 with col15:
     st.video("https://www.youtube.com/watch?v=iHkiQvxyr5c&t=2397s&pp=ygUedmFyaWFudCBjYWxsaW5nIGFuZCBhbm5vdGF0aW9u")
 
+
 st.markdown("## Lecture 4")
+col16,col17,col18,col19,col20 = st.columns(5)
 
-with col1:
-    pass
+with col16:
+    st.video("https://www.youtube.com/watch?v=y4Thnz0hFUc&pp=ygUUbG9uZyByZWFkIHNlcXVlbmNpbmc%3D")
 
-with col2:
-    pass
+with col17:
+    st.video("https://www.youtube.com/watch?v=mw2-jjuSFmE&t=2513s&pp=ygUUbG9uZyByZWFkIHNlcXVlbmNpbmfSBwkJfgkBhyohjO8%3D")
 
-with col3:
-    pass
+with col18:
+    st.video("https://www.youtube.com/watch?v=nFVSk1nvq2o&pp=ygUUbG9uZyByZWFkIHNlcXVlbmNpbmc%3D")
 
-with col4:
-    pass
+with col19:
+    st.video("https://www.youtube.com/watch?v=BC4s7SKV_Qc&pp=ygUUbG9uZyByZWFkIHNlcXVlbmNpbmc%3D")
 
-with col5:
-    pass
+with col20:
+    st.video("https://www.youtube.com/watch?v=lT3NqhKD840&pp=ygUUbG9uZyByZWFkIHNlcXVlbmNpbmc%3D")
 
 
 st.markdown("## Lecture 5")
+col21,col22,col23,col24,col25 = st.columns(5)
 
-with col1:
-    pass
+with col21:
+    st.video("https://www.youtube.com/watch?v=tlf6wYJrwKY&pp=ygUHcm5hLXNlcQ%3D%3D")
 
-with col2:
-    pass
+with col22:
+    st.video("https://www.youtube.com/watch?v=8lAVfKbRK3I&pp=ygUHcm5hLXNlcQ%3D%3D")
 
-with col3:
-    pass
+with col23:
+    st.video("https://www.youtube.com/watch?v=MniYgsZSp30&pp=ygUHcm5hLXNlcQ%3D%3D")
 
-with col4:
-    pass
+with col24:
+    st.video("https://www.youtube.com/watch?v=3hPyrQFTUuk&pp=ygUHcm5hLXNlcQ%3D%3D")
 
-with col5:
-    pass
+with col25:
+    st.video("https://www.youtube.com/watch?v=xh_wpWj0AzM&t=3s&pp=ygUHcm5hLXNlcQ%3D%3D")
 
 
 st.markdown("## Lecture 6")
+col26,col27,col28,col29,col30 = st.columns(5)
 
-with col1:
-    pass
+with col26:
+    st.video("https://www.youtube.com/watch?v=KY6SS4vRchY&pp=ygUEZ3NlYQ%3D%3D")
 
-with col2:
-    pass
+with col27:
+    st.video("https://www.youtube.com/watch?v=EF94wPaqXM0&t=481s&pp=ygUEZ3NlYQ%3D%3D")
 
-with col3:
-    pass
+with col28:
+    st.video("https://www.youtube.com/watch?v=egO7Lt92gDY&pp=ygUEZ3NlYQ%3D%3D")
 
-with col4:
-    pass
+with col29:
+    st.video("https://www.youtube.com/watch?v=Yi4d7JIlAsM&pp=ygUEZ3NlYQ%3D%3D")
 
-with col5:
-    pass
+with col30:
+    st.video("https://www.youtube.com/watch?v=Tm0LhciYxk8&pp=ygUEZ3NlYQ%3D%3D")
 
 
 st.markdown("## Lecture 7")
+col31,col32,col33,col34,col35 = st.columns(5)
 
-with col1:
-    pass
+with col31:
+    st.video("https://www.youtube.com/watch?v=LknDQw08P5w&t=1668s&pp=ygURc3BsaWNpbmcgYW5hbHlzaXM%3D")
 
-with col2:
-    pass
+with col32:
+    st.video("https://www.youtube.com/watch?v=2XC4drBx6RM&pp=ygURc3BsaWNpbmcgYW5hbHlzaXM%3D")
 
-with col3:
-    pass
+with col33:
+    st.video("https://www.youtube.com/watch?v=HqthkVAgmtg&pp=ygURc3BsaWNpbmcgYW5hbHlzaXM%3D")
 
-with col4:
-    pass
+with col34:
+    st.video("https://www.youtube.com/watch?v=TlLRsZOgHCo&pp=ygURc3BsaWNpbmcgYW5hbHlzaXM%3D")
 
-with col5:
-    pass
+with col35:
+    st.video("https://www.youtube.com/watch?v=31_J8M3wD78&pp=ygURc3BsaWNpbmcgYW5hbHlzaXM%3D")
+
 
 
 st.markdown("## Lecture 8")
+col36,col37,col38,col39,col40 = st.columns(5)
 
-with col1:
-    pass
+with col36:
+    st.video("https://www.youtube.com/watch?v=uFrrKHB9weY&t=621s&pp=ygUFc2NSTkE%3D")
 
-with col2:
-    pass
+with col37:
+    st.video("https://www.youtube.com/watch?v=k9VFNLLQP8c&pp=ygUFc2NSTkE%3D")
 
-with col3:
-    pass
+with col38:
+    st.video("https://www.youtube.com/watch?v=q--Hr9ZOpH4&pp=ygUFc2NSTkHSBwkJfgkBhyohjO8%3D")
 
-with col4:
-    pass
+with col39:
+    st.video("https://www.youtube.com/watch?v=xbX49h7BiUU&t=48s&pp=ygUFc2NSTkE%3D")
 
-with col5:
-    pass
+with col40:
+    st.video("https://www.youtube.com/watch?v=EPAzFAMYhoI&pp=ygUFc2NSTkE%3D")
 
 
 st.markdown("## Lecture 9")
+col41,col42,col43,col44,col45 = st.columns(5)
 
-with col1:
-    pass
+with col41:
+    st.video("https://www.youtube.com/watch?v=OcIazFGQv0g&pp=ygUYZG5hIG1ldGh5bGF0aW9uIGFuYWx5c2lz")
 
-with col2:
-    pass
+with col42:
+    st.video("https://www.youtube.com/watch?v=W-S84J4zK9E&pp=ygUYZG5hIG1ldGh5bGF0aW9uIGFuYWx5c2lz0gcJCX4JAYcqIYzv")
 
-with col3:
-    pass
+with col43:
+    st.video("https://www.youtube.com/watch?v=ojKA2xbyLYI&pp=ygUYZG5hIG1ldGh5bGF0aW9uIGFuYWx5c2lz0gcJCX4JAYcqIYzv")
 
-with col4:
-    pass
+with col44:
+    st.video("https://www.youtube.com/watch?v=e-hNHUcrScQ&pp=ygUYZG5hIG1ldGh5bGF0aW9uIGFuYWx5c2lz")
 
-with col5:
-    pass
+with col45:
+    st.video("https://www.youtube.com/watch?v=5NEoqa-k3xM&pp=ygUYZG5hIG1ldGh5bGF0aW9uIGFuYWx5c2lz")
 
 
 st.markdown("## Lecture 10")
+col46,col47,col48,col49,col50 = st.columns(5)
 
-with col1:
-    pass
+with col46:
+    st.video("https://www.youtube.com/watch?v=MD3Fc0XOjWk&pp=ygUQZXBpZ2VuZXRpYyBtYXJrcw%3D%3D")
 
-with col2:
-    pass
+with col47:
+    st.video("https://www.youtube.com/watch?v=2EDHQx00v3E&pp=ygUQZXBpZ2VuZXRpYyBtYXJrcw%3D%3D")
 
-with col3:
-    pass
+with col48:
+    st.video("https://www.youtube.com/watch?v=8y9u5VJZ6bA&pp=ygUXZXBpZ2VuZXRpYyBtYXJrcyBjb3Vyc2U%3D")
 
-with col4:
-    pass
+with col49:
+    st.video("https://www.youtube.com/watch?v=e8SlLdbSQ5g&pp=ygUXZXBpZ2VuZXRpYyBtYXJrcyBjb3Vyc2XSBwkJfgkBhyohjO8%3D")
 
-with col5:
-    pass
+with col50:
+    st.video("https://www.youtube.com/watch?v=KtOnBUMuZiY&t=2211s&pp=ygUXZXBpZ2VuZXRpYyBtYXJrcyBjb3Vyc2U%3D")
 
 
 st.markdown("## Lecture 11")
+col51,col52,col53,col54,col55 = st.columns(5)
 
-with col1:
-    pass
+with col51:
+    st.video("https://www.youtube.com/watch?v=B6SiIp2Cess&t=709s&pp=ygUMcGVhayBjYWxsaW5n")
 
-with col2:
-    pass
+with col52:
+    st.video("https://www.youtube.com/watch?v=hvE8ZhTKvWU&pp=ygUMcGVhayBjYWxsaW5n")
 
-with col3:
-    pass
+with col53:
+    st.video("https://www.youtube.com/watch?v=n6e6rdQsMmw&pp=ygUMcGVhayBjYWxsaW5n")
 
-with col4:
-    pass
+with col54:
+    st.video("https://www.youtube.com/watch?v=BACMdx5X3yQ&t=823s&pp=ygUbZGlmZmVyZW50aWFsIHBlYWsgYW5hbHlzaXMg")
 
-with col5:
-    pass
+with col55:
+    st.video("https://www.youtube.com/watch?v=XWcWn8dt4c8&pp=ygUbZGlmZmVyZW50aWFsIHBlYWsgYW5hbHlzaXMg")
 
 
 st.markdown("## Lecture 12")
+col56,col57,col58,col59,col60 = st.columns(5)
 
-with col1:
-    pass
+with col56:
+    st.video("https://www.youtube.com/watch?v=OJ7rXxkuvAM&pp=ygUMaGljIGFuYWx5c2lz0gcJCX4JAYcqIYzv")
 
-with col2:
-    pass
+with col57:
+    st.video("https://www.youtube.com/watch?v=0787ciSyrT8&t=3069s&pp=ygUMaGljIGFuYWx5c2lz")
 
-with col3:
-    pass
+with col58:
+    st.video("https://www.youtube.com/watch?v=p1VoktH-ygc&pp=ygUMaGljIGFuYWx5c2lz")
 
-with col4:
-    pass
+with col59:
+    st.video("https://www.youtube.com/watch?v=Hk5ixO7Tb24&pp=ygUMaGljIGFuYWx5c2lz")
 
-with col5:
-    pass
-
-
-st.markdown("## Lecture 13")
-
-with col1:
-    pass
-
-with col2:
-    pass
-
-with col3:
-    pass
-
-with col4:
-    pass
-
-with col5:
-    pass
+with col60:
+    st.video("https://www.youtube.com/watch?v=gRNO_GlJLuU&pp=ygUMaGljIGFuYWx5c2lz")
